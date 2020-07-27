44054dbca1497a8174987372245c459d589ecd82
Błażej Rybarkiewicz <b.rybarkiewicz@gmail.com>
2020-07-27 09:43:18 +0200

    App logo and favicon

--- src/assets/icons/icons.ts
+++ src/assets/icons/icons.ts
@@ -5,8 +5,11 @@ import {
   cilUser,
   cilLockLocked
 } from '@coreui/icons/js/free'
+import { logo, logoSquare } from './logo'
 
 export default {
+  logo,
+  logoSquare,
   cilSpeedometer,
   cilDescription,
   cilMagnifyingGlass,
--- src/containers/TheSidebar.vue
+++ src/containers/TheSidebar.vue
@@ -6,20 +6,17 @@
     @update:show="(value) => $store.commit('setSidebarShow', value)"
   >
     <CSidebarBrand class="d-md-down-none" to="/">
-      <h1>BBGH</h1>
       <CIcon
         class="c-sidebar-brand-full"
         name="logo"
         size="custom-size"
         :height="35"
-        viewBox="0 0 556 134"
       />
       <CIcon
         class="c-sidebar-brand-minimized"
-        name="logo"
+        name="logoSquare"
         size="custom-size"
         :height="35"
-        viewBox="0 0 110 134"
       />
     </CSidebarBrand>
 
