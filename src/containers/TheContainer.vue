e14f1a408acc890bd160cd7096ee7684839af588
Błażej Rybarkiewicz <b.rybarkiewicz@gmail.com>
2020-12-05 09:52:42 +0100

    Removed transition around router view

--- src/containers/TheContainer.vue
+++ src/containers/TheContainer.vue
@@ -6,9 +6,7 @@
       <div class="c-body">
         <main class="c-main">
           <CContainer fluid>
-            <transition name="fade" mode="out-in">
               <router-view :key="$route.path"></router-view>
-            </transition>
           </CContainer>
         </main>
       </div>
@@ -33,12 +31,4 @@ export default {
 </script>
 
 <style scoped>
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.15s;
-  }
-  .fade-enter,
-  .fade-leave-to {
-    opacity: 0;
-  }
 </style>
